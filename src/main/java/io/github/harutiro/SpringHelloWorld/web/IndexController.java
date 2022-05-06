be7f66bd275e87e91e5d5a8c6fbcf263a03bafd3
harutiro <harutiro2027@outlook.jp>
2022-05-06 18:06:08 +0900

    add: テンプレートエンジンの導入　UIとプログラムの分割

--- src/main/java/io/github/harutiro/SpringHelloWorld/web/IndexController.java
+++ src/main/java/io/github/harutiro/SpringHelloWorld/web/IndexController.java
@@ -3,15 +3,13 @@ package io.github.harutiro.SpringHelloWorld.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class IndexController {
 
     // GET //
     @GetMapping("/")
-    @ResponseBody  //戻り値がそのままレスポンスになる
     public String index(){
-        return "<h1>Hello,World!!</h1>";
+        return "index";
     }
 }
