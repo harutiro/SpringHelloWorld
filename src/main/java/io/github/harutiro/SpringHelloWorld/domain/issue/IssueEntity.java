3558354886abf2f8906b1f42afd034d87f6b2642
harutiro <harutiro2027@outlook.jp>
2022-05-06 18:36:44 +0900

    add: model と　Controller を作成する

---
+++ src/main/java/io/github/harutiro/SpringHelloWorld/domain/issue/IssueEntity.java
@@ -0,0 +1,25 @@
+package io.github.harutiro.SpringHelloWorld.domain.issue;
+
+public class IssueEntity {
+    private long id;
+    private String summary;
+    private String description;
+
+    public IssueEntity(long id, String summary, String description) {
+        this.id = id;
+        this.summary = summary;
+        this.description = description;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setSummary(String summary) {
+        this.summary = summary;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
