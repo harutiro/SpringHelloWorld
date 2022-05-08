1b0272911f0856ae1d1f6e140bae61ab8697b44b
harutiro <harutiro2027@outlook.jp>
2022-05-08 12:36:11 +0900

    fix: サービス層を作成して、ビジネスロジックを分割した。

---
+++ src/main/java/io/github/harutiro/SpringHelloWorld/domain/issue/IssueService.java
@@ -0,0 +1,14 @@
+package io.github.harutiro.SpringHelloWorld.domain.issue;
+
+import java.util.List;
+
+public class IssueService {
+
+    public List<IssueEntity> findAll(){
+        return List.of(
+                new IssueEntity(1,"概要1","説明1"),
+                new IssueEntity(2,"概要2","説明2"),
+                new IssueEntity(3,"概要3","説明3")
+        );
+    }
+}
--- src/main/java/io/github/harutiro/SpringHelloWorld/web/issue/IssueController.java
+++ src/main/java/io/github/harutiro/SpringHelloWorld/web/issue/IssueController.java
@@ -1,6 +1,7 @@
 package io.github.harutiro.SpringHelloWorld.web.issue;
 
 import io.github.harutiro.SpringHelloWorld.domain.issue.IssueEntity;
+import io.github.harutiro.SpringHelloWorld.domain.issue.IssueService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,15 +11,13 @@ import java.util.List;
 @Controller
 public class IssueController {
 
+    private final IssueService issueService = new IssueService();
+
     // GET // issues
     @GetMapping("/issues")
     public String showList(Model model){
-        var issueList = List.of(
-                new IssueEntity(1,"概要1","説明1"),
-                new IssueEntity(2,"概要2","説明2"),
-                new IssueEntity(3,"概要3","説明3")
-        );
-        model.addAttribute("issueList",issueList);
+
+        model.addAttribute("issueList",issueService.findAll());
         return "issues/list";
     }
 }
